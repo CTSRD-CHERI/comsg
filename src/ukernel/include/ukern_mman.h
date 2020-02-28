e49ea1079c826e3b6f80f08da58759089b4bd7ab
pentelbart <46610525+pentelbart@users.noreply.github.com>
2020-02-28 12:57:06 +0000

    Continued developing memory management subsystem

--- src/ukernel/include/ukern_mman.h
+++ src/ukernel/include/ukern_mman.h
@@ -1,30 +1,71 @@
 #ifndef _UKERN_MMAN_H
 #define _UKERN_MMAN_H
 
+#include "sys_comsg.h"
+#include "comesg_ukernel.h"
+
+#include <stdatomic.h>
 #include <sys/param.h>
+#include <cheri/cherireg.h>
 
 #define DEFAULT_BUFFER_SIZE 4096
+#define BUFFER_PERMS ( CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP | \
+	CHERI_PERM_STORE | CHERI_PERM_STORE_CAP )
 #define UKERN_MAP_LEN (1UL<<PDRSHIFT)
 #define UKERN_MMAP_FLAGS (\
 	MAP_ANON | MAP_SHARED | MAP_ALIGNED_CHERI \
-	| MAP_ALIGNED_SUPER )
-#define UKERN_MMAP_PROT (PROT_READ | PROT_WRITE)
+	| MAP_ALIGNED_SUPER | MAP_PREFAULT_READ )
+#define UKERN_RESERVE_FLAGS( UKERN_MMAP_FLAGS | MAP_GUARD )
+#define UKERN_EXTEND_FLAGS ( UKERN_MMAP_FLAGS | MAP_FIXED )
+#define UKERN_MMAP_PROT ( PROT_READ | PROT_WRITE )
+#define BUFFER_TABLE_SIZE (WORKER_COUNT * U_FUNCTIONS * MAX_COPORTS)
 
+#define RESERVE_PAGES 1
+#define RESERVE_SIZE ( RESERVE_PAGES * UKERN_MAP_LEN )
 
 #define MAP_UKERN(a,b) mmap(a,b,UKERN_MMAP_PROT,UKERN_MMAP_FLAGS,-1,0)
+#define EXTEND_UKERN(a,b) mmap(a,b,UKERN_MMAP_PROT,UKERN_EXTEND_FLAGS,-1,0)
+#define RESERVE_UKERN(a,b) mmap(a,b,UKERN_MMAP_PROT,UKERN_RESERVE_FLAGS,-1,0)
 
+#define REGION_MAPPED 1
+#define REGION_RESERVED 2
 
-
-struct buffer_table_entry
+typedef struct _buffer_table_entry
 {
 	int size;
-}
+	_ATOMIC int free;
+	int region_index;
+	void * buffer;
+} __attribute__((__aligned__(16))) buffer_table_entry_t;
 
-struct buffer_table
+typedef struct _buffer_table
 {
 	pthread_mutex_t lock;
 	int next;
 	int last;
+	int length;
+	buffer_table_entry * table;
+} buffer_table_t;
 
-}
+typedef struct _region_table_entry
+{
+	pthread_mutex_t lock;
+	void * mem;
+	int type;
+	int start_free;
+	int end_free;
+	int size;
+} region_table_entry_t;
+
+typedef struct _region_table
+{
+	pthread_mutex_t lock;
+	int next;
+	int last;
+	int length;
+	region_table_entry_t * table;
+} region_table_t;
+
+static buffer_table_t buffer_table; 
+
 #endif
--- src/ukernel/ukern_mman.c
+++ src/ukernel/ukern_mman.c
@@ -1,35 +1,124 @@
 #include <sys/mman.h>
 #include <sys/param.h>
+#include <cheri/cheric.h>
 
 #include <stddef.h>
+#include <pthread.h>
 
 #include "ukern_mman.h"
 
-int map_superpage(void ** map)
+static buffer_table_t buffer_table;
+static region_table_t region_table;
+
+
+int reserve_buffer_space()
 {
-	char * s_page;
+	void * s_page, *next_page;
 
 	unsigned int buffer_size;
+	unsigned int old_size;
 
-	s_page=MAP_UKERN(0,UKERN_MAP_LEN,UKERN_MMAP_PROT,UKERN_MMAP_FLAGS);
+	region_table_entry_t entry;
+
+	pthread_mutex_lock(region_table.lock);
+	old_size=region_table.length*UKERN_MAP_LEN;
+	region_table.table=realloc(region_table.table,old_size+ALLOCATION_SIZE);
+
+	s_page=MAP_UKERN(0,UKERN_MAP_LEN);
+	for (int i = 1; i <= RESERVE_PAGES; i++)
+	{
+		next_page=RESERVE_UKERN(s_page+(i*UKERN_MAP_LEN),UKERN_MAP_LEN);
+
+		entry.mem=next_page;
+		entry.type=REGION_RESERVED;
+		entry.start_free=0;
+		entry.end_free=0;
+		entry.size=UKERN_MAP_LEN;
+		pthread_mutex_init(&entry.lock,NULL);
+
+		region_table.table[region_table.next]=entry;
+		region_table.next++;
+	}
+	pthread_mutex_unlock(region_table.lock);
 	map=s_page;
 	return 0;
+}
+
+void * map_reservation(int index)
+{
+	void * mapped;
+	region_table_entry_t * region = &region_table.table[index];
+
+	pthread_mutex_lock(region_table.lock);
+	mapped=EXTEND_UKERN(region->mem,region->size);
+	region->mem=mapped;
+	pthread_mutex_unlock(region_table.unlock);
+
 }
 
 int allocate_buffers(void ** buffer_array)
 {
 	void * raw_mem_page, * buf_cap, * raw_mem_cursor;
-	int error = map_superpage(&raw_mem);
+	int error = reserve_buffer_space(&raw_mem);
 	int buf_count = UKERN_MAP_LEN/DEFAULT_BUFFER_SIZE;
 	raw_mem_cursor=raw_mem_page;
 	for (int i = 0; i < buf_count; i++)
 	{
-		buf_cap=cheri_csetbounds(raw_mem_cursor,);
+		buf_cap=cheri_csetbounds(raw_mem_cursor,DEFAULT_BUFFER_SIZE);
+		buf_cap=cheri_andperms(buf_cap,BUFFER_PERMS);
 		raw_mem_cursor=cheri_setoffset(raw_mem_page,DEFAULT_BUFFER_SIZE);
-		
+		buffer_array[i]=buf_cap;
+	}
+}
+
+void * find_available_memory(size_t len)
+{
+	for (int i = 0; i < region_table.length; ++i)
+	{
+		if(region_table.type==REGION_RESERVED)
+		{
+
+		}
+		if region_table.table[i].sta
+	}
 }
 
 void * buffer_malloc(size_t len)
 {
+	// get 
+	pthread_mutex_lock(buffer_table.lock);
+	if (buffer_table.next==buffer_table.last)
+	{
+		MAP_UKERN(buffer_table.table[length],UKERN_EXTEND_FLAGS);
+	}
+	pthread_mutex_unlock(buffer_table.lock);
+	return 0;
+}
 
+int buffer_table_setup(void)
+{
+	pthread_mutexattr_t mtx_attr;
+	pthread_mutexattr_init(&mtx_attr);
+	pthread_mutexattr_settype(&mtx_attr,RECURSIVE);
+	
+	buffer_table.length=0;
+	buffer_table.next=0;
+	buffer_table.last=0;
+	buffer_table.table=NULL;
+
+	return pthread_mutex_init(&buffer_table.lock,&mtx_attr);
+}
+
+int region_table_setup(void)
+{
+	pthread_mutexattr_t mtx_attr;
+	pthread_mutexattr_init(&mtx_attr);
+	pthread_mutexattr_settype(&mtx_attr,RECURSIVE);
+	
+	region_table.length=0;
+	region_table.next=0;
+	region_table.last=0;
+	region_table.table=NULL;
+
+	return pthread_mutex_init(&buffer_table.lock,&mtx_attr);
 }
