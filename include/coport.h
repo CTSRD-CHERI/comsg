5781012419a7ba659ea9ca7db32bb4ae376ea3ce
pentelbart <46610525+pentelbart@users.noreply.github.com>
2020-02-29 21:10:44 +0000

    Update coport.h

--- include/coport.h
+++ include/coport.h
@@ -23,6 +23,8 @@
 				can obtain a read-only capability. sender loses write capability
 				to the sent message
 	COPIPE	  -	synchronous message passing ipc. as yet unimplemented.
+				might be a blocking one with no buffer using call into ukern
+				on both sides to copy straight from dest to source
 */
 
 typedef enum {COCHANNEL, COCARRIER, COPIPE} coport_type_t;
