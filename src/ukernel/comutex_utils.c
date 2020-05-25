05e4f5162e3ce21949231e3c6778a241bb1e878f
pentelbart <46610525+pentelbart@users.noreply.github.com>
2020-05-25 22:16:28 +0100

    Major refactor
    Microkernel code is now much less monolithic. Makefile not yet updated to take account of these changes, however.

---
+++ src/ukernel/comutex_utils.c
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2020 Peter S. Blandford-Baker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+	This file contains functions for comutexes, a feature I initially conceived
+	to allow cross-process synchronisation. I found that it was not essential
+	for the MPhil as I focused in more and more on the IPC model, but could be
+	useful if developed further and might solve some stability problems if
+	sufficiently well implemented. -PBB
+*/
+
+//NOTE-PBB: Many of these functions, if used, would be better situated in the
+//relevant ukern_* files for the subsystems they pertain to.
+
+int comutex_tbl_setup(void)
+{
+    pthread_mutexattr_t lock_attr;
+    pthread_mutexattr_init(&lock_attr);
+    pthread_mutexattr_settype(&lock_attr,PTHREAD_MUTEX_RECURSIVE);
+    int error=pthread_mutex_init(&comutex_table.lock,&lock_attr);
+    comutex_table.index=0;
+    comutex_table.table=ukern_malloc(COMTX_TBL_LEN);
+    mlock(comutex_table.table,COMTX_TBL_LEN);
+    /* reserve a superpage or two for this, entries should be small */
+    /* reserve a few superpages for ports */
+    return error;
+}
+
+
+int comutex_deinit(comutex_tbl_entry_t * m)
+{
+    sys_comutex_t mtx = m->mtx;
+    free(mtx.kern_mtx->lock);
+    free(mtx.kern_mtx);
+    mtx.user_mtx=NULL;
+
+    // remove from table?
+    return 0;
+}
+
+void create_comutex(comutex_t * cmtx,char * name)
+{
+    int error;
+    sys_comutex_t * sys_mtx;
+    comutex_tbl_entry_t table_entry;
+
+    sys_mtx=ukern_malloc(sizeof(sys_comutex_t));
+    error=sys_comutex_init(name,sys_mtx);
+    
+    table_entry.id=generate_id();
+    table_entry.mtx=*sys_mtx;
+
+    cmtx->mtx=sys_mtx->user_mtx;
+    strcpy(cmtx->name,name);
+
+    error=add_mutex(table_entry);
+
+    return;
+}
+
+void *comutex_setup(void *args)
+{
+    worker_args_t * data=args;
+    cocall_comutex_init_t comutex_args;
+    comutex_tbl_entry_t table_entry;
+    sys_comutex_t * mtx;
+
+    int error;
+    int index;
+    int lookup;
+
+    void * __capability sw_code;
+    void * __capability sw_data;
+    void * __capability caller_cookie;
+    void * __capability target;
+    
+
+    error=coaccept_init(&sw_code,&sw_data,data->name,&target);
+    data->cap=target;
+    update_worker_args(data,U_COMUTEX_INIT);
+    for (;;)
+    {
+        error=coaccept(sw_code,sw_data,&caller_cookie,&comutex_args,sizeof(comutex_args));
+        /* check args are acceptable */
+
+        /* check if mutex exists */
+        pthread_mutex_lock(&comutex_table.lock);
+        lookup=lookup_mutex(comutex_args.args.name,&mtx);
+        if(lookup==1)
+        {
+            /* if it doesn't, set up mutex */
+            mtx=ukern_malloc(sizeof(sys_comutex_t));
+            error=sys_comutex_init(comutex_args.args.name,mtx);
+            table_entry.mtx=*mtx;
+            table_entry.id=generate_id();
+            index=add_mutex(table_entry);
+            if(error!=0)
+            {
+                err(1,"unable to init_port");
+            }
+        }
+        pthread_mutex_unlock(&comutex_table.lock);
+        strcpy(comutex_args.mutex->name,mtx->name);
+        comutex_args.mutex->mtx=mtx->user_mtx;
+        comutex_args.mutex->key=NULL;
+        
+    }
+    return 0;
+}
+
+void *comutex_lock(void *args)
+{
+    worker_args_t * data=args;
+    colock_args_t colock_args;
+    sys_comutex_t * mtx;
+    comutex_t * user_mutex;
+
+    int error;
+    int lookup;
+
+    void * __capability sw_code;
+    void * __capability sw_data;
+    void * __capability caller_cookie;
+    void * __capability target;
+
+    error=coaccept_init(&sw_code,&sw_data,data->name,&target);
+    data->cap=target;
+    update_worker_args(data,U_COLOCK);
+    for (;;)
+    {
+        error=coaccept(sw_code,sw_data,&caller_cookie,&colock_args,sizeof(colock_args));
+        /* check args are acceptable */
+        // validation
+        /* check if mutex exists */
+        lookup=lookup_mutex(colock_args.mutex->name,&mtx);
+        if(lookup==0)
+        {
+            user_mutex=colock_args.mutex;
+            error=sys_colock(mtx,user_mutex->key);
+            mtx->key=user_mutex->key;
+        }
+        //report errors
+    }
+    return 0;
+}
+
+void *comutex_unlock(void *args)
+{
+    void * __capability sw_code;
+    void * __capability sw_data;
+    void * __capability  caller_cookie;
+    void * __capability  target;
+
+    worker_args_t * data=args;
+    counlock_args_t colock_args;
+    sys_comutex_t * mtx;
+    comutex_t * user_mutex;
+
+    int error;
+    int lookup;
+
+    error=coaccept_init(&sw_code,&sw_data,data->name,&target);
+    data->cap=target;
+    update_worker_args(data,U_COUNLOCK);
+    for (;;)
+    {
+        error=coaccept(sw_code,sw_data,&caller_cookie,&colock_args,sizeof(colock_args));
+        /* check args are acceptable */
+        // validation
+        /* check if mutex exists */
+        lookup=lookup_mutex(colock_args.mutex->name,&mtx);
+        if(lookup==0)
+        {
+            user_mutex=colock_args.mutex;
+            error=sys_counlock(mtx,user_mutex->key);
+            mtx->key=user_mutex->key;
+        }
+        //report errors
+    }
+    return 0;
+}
+
+int add_mutex(comutex_tbl_entry_t entry)
+{
+    int entry_index;
+    
+    pthread_mutex_lock(&comutex_table.lock);
+    if(comutex_table.index>=MAX_COPORTS)
+    {
+        pthread_mutex_unlock(&comutex_table.lock);
+        return 1;
+    }
+    comutex_table.table[comutex_table.index]=entry;
+    entry_index=comutex_table.index++;
+    pthread_mutex_unlock(&comutex_table.lock);
+    return entry_index;
+}
+
+int lookup_mutex(char * mtx_name,sys_comutex_t ** mtx_buf)
+{
+    if (strlen(mtx_name)>COPORT_NAME_LEN)
+    {
+        err(1,"mtx name length too long");
+    }
+    for(int i = 0; i<comutex_table.index;i++)
+    {
+        if (comutex_table.table[i].mtx.user_mtx==NULL)
+        {
+            *mtx_buf=NULL;
+            return 1;
+        }
+        if(strcmp(mtx_name,comutex_table.table[i].mtx.name)==0)
+        {
+            *mtx_buf=&comutex_table.table[i].mtx;
+            return 0;
+        }
+    }
+    *mtx_buf=NULL;
+    return 1;
+}
---
+++ src/ukernel/ukern_requests.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2020 Peter S. Blandford-Baker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "ukern/ukern_mman.h"
+#include "ukern/ukern_requests.h"
+
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+int coaccept_init(
+    void * __capability * __capability  code_cap,
+    void * __capability * __capability  data_cap, 
+    const char * target_name,
+    void * __capability * __capability target_cap)
+{
+    int error;
+    error=cosetup(COSETUP_COACCEPT,code_cap,data_cap);
+    if (error!=0)
+    {
+        err(errno,"ERROR: Could not cosetup.\n");
+    }
+
+    error=coregister(target_name,target_cap);
+    if (error!=0)
+    {
+        err(errno,"ERROR: Could not coregister with name %s.\n",target_name);
+    }
+    return (error);
+}
+
+
+void update_worker_args(worker_args_t * args, const char * function_name)
+{
+    int i,j;
+    char lookup_string[LOOKUP_STRING_LEN];
+    strncpy(lookup_string,args->name,LOOKUP_STRING_LEN);
+    for(i = 0; i<=U_FUNCTIONS; i++)
+    {
+        if (strcmp(worker_map[i].func_name,function_name)==0)
+        {
+            break;
+        }
+        else if (i==U_FUNCTIONS)
+        {
+            err(1,"function name %s not found in map",function_name);
+        }
+    }
+    for(j = 0; j<=WORKER_COUNT;j++)
+    {
+        if (strncmp(worker_map[i].workers[j].name,args->name,LOOKUP_STRING_LEN)==0)
+        {
+            worker_map[i].workers[j].cap=args->cap;
+            return;
+        }
+        else if (j==WORKER_COUNT)
+        {
+            err(1,"worker %s not found in map",args->name);
+        }
+    }
+}
+
+void *manage_requests(void *args)
+{
+    int error;
+    int workers_index;
+
+    request_handler_args_t * data = args;
+
+    void * __capability sw_code;
+    void * __capability sw_data;
+    void * __capability cookie = 0;
+    void * __capability target;
+
+    worker_args_t * workers;
+
+    cocall_lookup_t * lookup;
+
+    error=coaccept_init(&sw_code,&sw_data,data->func_name,&target);
+    workers_index=-1;
+    for(int i = 0; i < U_FUNCTIONS; i++)
+    {
+        if(strcmp(worker_map[i].func_name,data->func_name)==0)
+        {
+            workers=worker_map[i].workers;
+            workers_index=i;
+            break;
+        }
+    }
+    if(workers_index==-1)
+    {
+        err(1,"Function workers not registered");
+    }
+    lookup=ukern_malloc(sizeof(cocall_lookup_t));
+    for(;;)
+    {
+        for(int j = 0; j < WORKER_COUNT; j++)
+        {
+            //printf("coaccepting for %s\n",data->func_name);
+            error=coaccept(sw_code,sw_data,&cookie,lookup,sizeof(cocall_lookup_t));
+            //printf("Lookup of %s is size %lu",workers[j].name,sizeof(cocall_lookup_t));
+            lookup->cap=workers[j].cap;
+        }
+    }
+    ukern_free(lookup);
+}
+
+int spawn_workers(void * func, pthread_t * threads, const char * name)
+{
+    pthread_attr_t thread_attrs;
+    worker_args_t * args;
+    int e;
+    int w_i;
+    char thread_name[LOOKUP_STRING_LEN];
+    bool private;
+
+    if (name[0]=='_')
+    	private=true;
+    else
+    	private=false;
+    /* split into threads */
+    pthread_mutex_lock(&worker_map_lock);
+    if (!private)
+    {
+        w_i=atomic_fetch_add(&next_worker_i,1);
+    	strcpy(worker_map[w_i].func_name,name);
+    }
+    else
+    {
+    	w_i=atomic_fetch_add(&next_priv_worker_i,1);
+    	strcpy(private_worker_map[w_i].func_name,name);
+    }
+    //  printf("workers for %s\n",name);
+    pthread_attr_init(&thread_attrs);
+    for (int i = 0; i < WORKER_COUNT; i++)
+    {
+        args=ukern_malloc(sizeof(worker_args_t));
+        rand_string(&thread_name[0],THREAD_STRING_LEN);
+        strcpy(args->name,thread_name);
+        args->cap=NULL;
+        //printf("%s",thread_name);
+        
+        //printf("thr_name %s\n",args->name);
+        e=pthread_create(&threads[i],&thread_attrs,func,args);
+        if (e==0)
+        {
+        	if (!private)
+            {
+            	memcpy(&worker_map[w_i].workers[i],args,sizeof(worker_args_t));
+            }
+            else
+            {
+            	memcpy(&private_worker_map[w_i].workers[i],args,sizeof(worker_args_t));
+            }
+        }
+    }
+    pthread_mutex_unlock(&worker_map_lock);
+    return 0;
+}
