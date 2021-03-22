3fb58815a2a430701f391aee6f99f7f7de2ad330
pentelbart <46610525+pentelbart@users.noreply.github.com>
2021-03-22 11:22:18 +0000

    Fix missing curly braces

--- example-setup.sh
+++ example-setup.sh
@@ -1,7 +1,7 @@
 # Should be sourced!
 
 CHERI_ROOT=~/cheri/
-cheri_sdk_dir="$(CHERI_ROOT)/output/sdk"
+cheri_sdk_dir="${CHERI_ROOT}/output/sdk"
 PATH="${cheri_sdk_dir}/bin:${PATH}"
 CC=clang-11
 cc=$CC
