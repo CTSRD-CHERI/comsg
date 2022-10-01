b3bff07490b7c83624a291f00959d1a054ad6158
Peter Blandford-Baker <46610525+pentelbart@users.noreply.github.com>
2022-10-01 12:56:56 +0100

    Ensure source directory is properly included in build

--- comsg.src.mk
+++ comsg.src.mk
@@ -3,7 +3,7 @@ OUT_PATH := $(OUT_DIR)/$(ARCH)
 
 OBJS := $(SRCS:%=$(BUILD_PATH)/%.o)
 DEPS := $(OBJS:.o=.d)
-CFLAGS += -MMD -MP
+CFLAGS += -MD -MP
 CONFIG := -target riscv64-unknown-freebsd13 --sysroot=$(CHERI_ROOT)/output/rootfs-riscv64-purecap -B$(cheri_sdk_dir)/bin -march=rv64imafdcxcheri -mabi=l64pc128d -mno-relax
 LDFLAGS += -L $(OUT_PATH)
 
@@ -27,11 +27,11 @@ $(OUT_PATH):
 
 .SECONDEXPANSION:
 $(BUILD_PATH)/%.c.o: %.c | $(BUILD_PATH)/. $$(@D)/.
-	$(CC) $(CONFIG) $(INC_FLAGS) $(CFLAGS) -c $< -o $@
+	$(CC) $(CONFIG) $(INC_FLAGS) -I$(realpath $(<D)) $(CFLAGS) -c $< -o $@
 
 .SECONDEXPANSION:
 $(BUILD_PATH)/%.S.o: %.S | $(BUILD_PATH)/. $$(@D)/.
-	$(CC) $(CONFIG) $(INC_FLAGS) $(CFLAGS) -c $< -o $@
+	$(CC) $(CONFIG) $(INC_FLAGS) -I$(realpath $(<D)) $(CFLAGS) -c $< -o $@
 
 $(TGT): $(OBJS) | $(OUT_PATH)
 	$(CC) $(CONFIG) $(CFLAGS) $(LDFLAGS) $(OBJS) -o $(OUT_PATH)/$(OUT_FILE) 
